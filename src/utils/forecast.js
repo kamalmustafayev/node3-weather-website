fd2c67991056c186900f0386bcfdb30cc6c6b8a6
kamalmustafayev <42323286+kamalmustafayev@users.noreply.github.com>
2019-08-19 09:42:00 +0400

    High and low temp added

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,7 +9,7 @@ const forecast = (latitude, longitude, callback) => {
         }else if(body.error){
             callback("The given location (or time) is invalid", undefined)
         }else{
-            callback(undefined, body.daily.data[0].summary + " It is currently " +  body.currently.temperature + " degrees out. There is a " + body.currently.precipProbability + "% chance of rain.")
+            callback(undefined, body.daily.data[0].summary + " It is currently " +  body.currently.temperature + " degrees out. The high today is " + body.daily.data[0].temperatureHigh + ". The low is " + body.daily.data[0].temperatureLow + ". There is a " + body.currently.precipProbability*100 + "% chance of rain.")
         }
     })
 }
